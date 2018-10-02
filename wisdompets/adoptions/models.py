bfdd1c89192f4779707b3a86410aca5cdacffc69
liberbell <liberbell@gmail.com>
2018-10-03 08:36:58 +0900

    models1

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
+    species = models.CharField(max_length=30)
+    bread = models.CharField(max_length=30, blank=True)
+    description = models.TextField()
+    sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
+    submition_date = models.DateTimeField()
+    age = models.IntegerField(null=True)
