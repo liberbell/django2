edc2cbcdb21d494f115f1319ef186cc95367d10b
liberbell <liberbell@gmail.com>
2018-10-05 09:04:05 +0900

    models3

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -4,8 +4,8 @@ class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
     name = models.CharField(max_length=100)
     submitter = models.CharField(max_length=100)
-    species = models.CharField(max_length=30)
-    bread = models.CharField(max_length=30, blank=True)
+    speices = models.CharField(max_length=30)
+    breed = models.CharField(max_length=30, blank=True)
     description = models.TextField()
     sex = models.CharField(choices=SEX_CHOICES, max_length=1, blank=True)
     submission_date = models.DateTimeField()
